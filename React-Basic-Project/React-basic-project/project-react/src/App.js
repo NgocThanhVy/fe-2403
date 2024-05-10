81bde98d78dd78147ff60226cb4c46e475c65d80
NgocThanhVy <thngoc1010@gmail.com>
2024-05-10 13:17:24 +0700

    project basic reactJS

---
+++ React-Basic-Project/React-basic-project/project-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UserPage from './component/React-basic-project/UserPage';
+
+function App() {
+  return (
+    <div className="App">
+      <UserPage />
+    </div>
+  );
+}
+
+export default App;
